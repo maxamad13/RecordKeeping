daf6cff93a8928fa0c322dd04380b8ae3872dc6c
Mohamed Sharif <mohame@gmail.com>
2016-05-28 09:20:09 -0500

    first commit

---
+++ RecordKeeping/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecordKeeping.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string LastName { get; set; }
+        public string Firstname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
